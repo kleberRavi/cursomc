4c2046d4b2fc44febdb4d2fc2c6b91497c2d86c9
kleberRavi <kfd7cvp@gmail.com>
2019-08-20 20:43:38 -0100

    Criando repository e service para Categoria

---
+++ src/main/java/com/kleber/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.kleber.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kleber.cursomc.domain.Categoria;
+import com.kleber.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}
