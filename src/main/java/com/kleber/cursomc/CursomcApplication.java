68da5d7644fa3c7c4360d17c53031a7ec8842d76
kleberRavi <kfd7cvp@gmail.com>
2019-08-20 21:01:02 -0100

    Criando operação de instanciação

--- src/main/java/com/kleber/cursomc/CursomcApplication.java
+++ src/main/java/com/kleber/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.kleber.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.kleber.cursomc.domain.Categoria;
+import com.kleber.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
