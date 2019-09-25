6d809d83eb0810fb90d413100b8958050d6124a7
kleberRavi <kfd7cvp@gmail.com>
2019-09-25 20:49:44 -0100

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/kleber/cursomc/domain/Produto.java
+++ src/main/java/com/kleber/cursomc/domain/Produto.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -30,13 +31,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	public List<Pedido> getPedidos(){
-		List<Pedido> lista = new ArrayList<>();
-		for (ItemPedido x : itens) {
-			lista.add(x.getPedido());
-		}
-		return lista;
-	}
+	
 	
 	@JsonBackReference
 	@ManyToMany
@@ -46,7 +41,7 @@ public class Produto implements Serializable {
 			)
 	private List<Categoria> categorias = new ArrayList<>();
 	
-	
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
@@ -61,6 +56,15 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	@JsonIgnore
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
